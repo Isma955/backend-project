a17d76250e231eff4dadda42eea997bc0ed123e0
Sherlock0595 <magomed.umaev@mail.ru>
2023-07-28 11:19:40 +0300

    Добавил удаление и изменение пользователя

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/registration", userController.registration); // Регистрация пользователя
 router.post("/login", userController.login); // Вход в учетную запись
 router.get("/user", authMiddleware, userController.getUser); // вывод пользователя
-
+router.patch("/user/update", authMiddleware, userController.updateUser); // изменение данных
+router.delete("/user/delete", authMiddleware, userController.deleteUser); // удаление пользователя
 module.exports = router;
